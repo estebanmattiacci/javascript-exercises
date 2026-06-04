734883e0bacce4f7e9208ac92ae8ce6bd218ccc9
esteb <esteb.mattia36@gmail.com>
2026-06-04 14:33:07 +0200

    exercises done js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // const strArr = Array.from(str);
+    // const n = strArr.length;
+    // for (let i = 0; i < (n / 2); i++) {
+    //     const toReplacePos = (n - 1) - i;
+    //     [strArr[i], strArr[toReplacePos]] = [strArr[toReplacePos], strArr[i]];
+    // }
+    // return strArr.reduce((result, currentValue) => result + currentValue);
+    return str.split("").reverse().join("")
 };
 
 // Do not edit below this line
