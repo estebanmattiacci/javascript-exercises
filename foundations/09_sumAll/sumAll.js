affda35f22c776931fabd18a3f1b8e8b0b752551
Esteban <esteb.mattia36@gmail.com>
2026-06-06 18:50:07 +0100

    sum all

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || Math.sign(num1) < 0 || Math.sign(num2) < 0) {
+        return "ERROR";
+    }
+    const max = Math.max(num1, num2);
+    const min = Math.min(num1, num2);
 
+    let counter = min;
+    for (let i = min + 1; i < max + 1; i++) {
+        counter += i;
+    }
+    return counter;
 };
 
 // Do not edit below this line
